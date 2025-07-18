4ba985c5a66d344629dd313a708e39de3b50f0f6
skrodrigo <rodrigoa0987@gmail.com>
2025-07-18 10:05:44 -0300

    fix: prisma

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,8 +1,13 @@
-import { withOptimize } from "@prisma/extension-optimize";
+import { withAccelerate } from "@prisma/extension-accelerate";
 import { PrismaClient } from "@/generated/prisma";
 
-const prisma = new PrismaClient().$extends(
-	withOptimize({ apiKey: process.env.OPTIMIZE_API_KEY as string }),
-);
+const globalForPrisma = global as unknown as {
+	prisma: PrismaClient;
+};
+
+const prisma =
+	globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate());
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
 export default prisma;
